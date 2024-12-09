f4f9f1799525a79cb8783cbad167ab76d8a5613b
HarryZ <uqbarz@gmail.com>
2024-12-09 15:05:29 +0800

    [feat] new dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-"use client"
+"use client";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import DDPic from '@/static/img/dd.png';
 
 export default function Home() {
   const [query, setQuery] = useState("");
@@ -13,133 +15,48 @@ export default function Home() {
         router.push(`/compare/${encodeURIComponent(query)}`);
       }
     }
-  }
+  };
+
   return (
     <div className="h-full">
-      <div className="font-banner">BibiPrice</div>
-      <div className="relative">
-        <span className="sr-only">From Uiverse.io by seyed-mohsen-mousavi</span>
+      <div
+        className="-z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center w-full h-full
+                      has-[:focus]:bg-opacity-40 has-[:focus]:backdrop-blur-sm has-[:focus]:-z-0"
+      >
         <input
           placeholder="Search..."
-          className="input shadow-lg focus:border-2 border-gray-300 px-5 py-3 rounded-xl w-56 transition-all focus:w-64 outline-none"
+          className="z-10 input shadow-lg focus:border-2 border-gray-300 px-5 py-3 rounded-xl w-80 h-12 transition-all focus:w-96 outline-none"
           name="search"
           type="search"
           onKeyDown={handleKeyDown}
           onChange={(e) => setQuery(e.target.value)}
           value={query}
         />
-        <svg
-          className="size-6 absolute top-3 right-3 text-gray-500"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          ></path>
-        </svg>
       </div>
+      <div className="-z-40 absolute pt-16 h-screen w-full">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] h-[90%] bg-[#a0ae9d] rounded-[5rem] grid grid-cols-2">
+          <div className="flex flex-col ml-14 mt-14 w-full">
+            <div className="font-staatliches text-9xl text-[#fefefe]">
+              BIBI PRICE
+            </div>
+            <div className="font-staatliches text-4xl text-[#fefefe]">
+              fancy price tracker for e-commerce websites
+            </div>
+          </div>
+          <div className="absolute bottom-14 right-14 flex flex-row gap-8 items-end">
+            <div className="w-80 h-60 bg-[#fefefe] rounded-3xl"></div>
+            <div className="w-80 h-40 bg-[#fefefe] rounded-3xl p-4 flex flex-col justify-end">
+              <div className="font-semibold">ZJU B/S Project</div>
+              <div className="font-semibold">2024-10</div>
+              {/* <Image src={DDPic} alt="dd-logo" width={96} height={96}></Image> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="-z-20 absolute pt-16 top-0 left-0 w-full h-full min-h-screen grid grid-rows-2 justify-center">
+        <div></div>
+      </div> */}
+      {/* <div className="font-banner">BibiPrice</div> */}
     </div>
   );
 }
-
-
-{/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-  <Image
-    className="dark:invert"
-    src="https://nextjs.org/icons/next.svg"
-    alt="Next.js logo"
-    width={180}
-    height={38}
-    priority
-  />
-  <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-    <li className="mb-2">
-      Get started by editing{" "}
-      <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-        src/app/page.tsx
-      </code>
-      .
-    </li>
-    <li>Save and see your changes instantly.</li>
-  </ol>
-
-  <div className="flex gap-4 items-center flex-col sm:flex-row">
-    <a
-      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <Image
-        className="dark:invert"
-        src="https://nextjs.org/icons/vercel.svg"
-        alt="Vercel logomark"
-        width={20}
-        height={20}
-      />
-      Deploy now
-    </a>
-    <a
-      className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-      href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Read our docs
-    </a>
-  </div>
-</main>
-<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-  <a
-    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      aria-hidden
-      src="https://nextjs.org/icons/file.svg"
-      alt="File icon"
-      width={16}
-      height={16}
-    />
-    Learn
-  </a>
-  <a
-    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      aria-hidden
-      src="https://nextjs.org/icons/window.svg"
-      alt="Window icon"
-      width={16}
-      height={16}
-    />
-    Examples
-  </a>
-  <a
-    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      aria-hidden
-      src="https://nextjs.org/icons/globe.svg"
-      alt="Globe icon"
-      width={16}
-      height={16}
-    />
-    Go to nextjs.org →
-  </a>
-</footer>
-</div> */}
